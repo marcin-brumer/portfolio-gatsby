fd1a054caa3add1b25d15754a3c9b3c846cb79ff
Marcin Brumer <brumek31@gmail.com>
2019-04-11 16:34:08 +0200

    Added media queries

---
+++ src/utils/media.js
@@ -0,0 +1,8 @@
+export const media = {
+  mobileS: `(max-width: 350px)`,
+  mobileL: `(max-width: 500px)`,
+  tablet: `(max-width: 768px)`,
+  laptop: `(max-width: 1200px)`,
+  laptopL: `(max-width: 1500px)`,
+  desktop: `(min-width: 1501px)`,
+};
